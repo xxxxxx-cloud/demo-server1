22d34965e25d74d638ee85017210f2e4f9b02cc7
jinshiqiang <18710486944@163.com>
2020-10-27 10:31:34 +0800

    0.0

--- src/main/java/com/xxxxx/demo/DemoController.java
+++ src/main/java/com/xxxxx/demo/DemoController.java
@@ -3,16 +3,14 @@ package com.xxxxx.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author 金世强
+ */
 @RestController
 public class DemoController {
 
     @GetMapping("getDemo1")
     public String getDemo1() {
-//        try {
-//            Thread.sleep(1000*65);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
         System.out.println("................0.0..........");
         return "Hello!";
     }
