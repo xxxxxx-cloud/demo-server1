0d0d628110f469a70342fcdb9272f7e0cd92f777
jinshiqiang <18710486944@163.com>
2020-10-29 09:47:07 +0800

    初始化

--- demo-server1-web/src/main/java/com/xxxxx/DemoServer1WebApplication.java
+++ demo-server1-web/src/main/java/com/xxxxx/DemoServer1WebApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+/**
+ * @author jinshiqiang
+ */
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
